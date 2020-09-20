b8a2d62dbe58f304d91eb5a6d2d51075231300e1
masagrator <masagrator@gmail.com>
2020-09-20 17:58:22 +0200

    Fix bugs

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,8 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include <switch.h>
 
-#include <stdlib.h>
-
 #include <borealis.hpp>
+
+#include "About_tab.hpp"
 
 // works
 Result pctlDeleteParentalControls(void)
@@ -19,16 +16,6 @@ Result pctlDeletePairing(void)
 	return serviceDispatch(pctlGetServiceSession_Service(), 1941);
 }
 
-void init_app(void)
-{
-	pctlInitialize();
-}
-
-void exit_app(void)
-{
-	pctlExit();
-}
-
 int main(int argc, char *argv[])
 {
 	// Init the app
@@ -40,12 +27,12 @@ int main(int argc, char *argv[])
 	}
 
 	// Create a sample view
+	pctlInitialize();
 	brls::TabFrame* rootFrame = new brls::TabFrame();
 	rootFrame->setTitle("Reset Parental Controls");
 	rootFrame->setIcon(BOREALIS_ASSET("icon.jpg"));
 	
 	brls::List* OptionsList = new brls::List();
-	brls::List* AboutList = new brls::List();
 
 	brls::ListItem* RegisterPasscode = new brls::ListItem("Register parental control PIN");
 	RegisterPasscode->getClickEvent()->subscribe([](brls::View* view) {
@@ -88,11 +75,7 @@ int main(int argc, char *argv[])
 	brls::ListItem* DeletePairing = new brls::ListItem("Delete parental control app pairing");
 	DeletePairing->getClickEvent()->subscribe([](brls::View* view) {
 		Result rc = pctlDeletePairing();
-		if (rc == 0xE401) {
-			std::string notification = "Parental control app is not paired.";
-			brls::Application::notify(notification);
-		}
-		else if (R_FAILED(rc)) {
+		if (R_FAILED(rc)) {
 			std::string notification = "Process canceled.";
 			brls::Application::notify(notification);
 		}
@@ -106,16 +89,7 @@ int main(int argc, char *argv[])
 	
 	rootFrame->addTab("Options", OptionsList);
 	
-    brls::Label* Label1 = new brls::Label(brls::LabelStyle::REGULAR, "Reset Parental Controls 1.1.1", false);
-    AboutList->addView(Label1);
-
-    brls::Label* Label2 = new brls::Label(brls::LabelStyle::MEDIUM, "Research & Development: TotalJustice\nPort to Borealis UI: MasaGratoR", true);
-    AboutList->addView(Label2);
-	
-	brls::Label* Label3 = new brls::Label(brls::LabelStyle::DESCRIPTION, "Copyright (C) 2020\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses/", true);
-    AboutList->addView(Label3);
-	
-	rootFrame->addTab("About", AboutList);
+	rootFrame->addTab("About", new AboutTab());
 
 
 	// Add the root view to the stack
@@ -125,5 +99,6 @@ int main(int argc, char *argv[])
 	while (brls::Application::mainLoop());
 
 	// Exit
+	pctlExit();
 	return EXIT_SUCCESS;
 }
